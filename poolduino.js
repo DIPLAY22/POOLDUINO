2554e6c11ecdef4c382891608d10735f4971e6e1
DIPLAY22 <diplay22@gmail.com>
2016-11-24 02:17:22 -0200

    Create poolduino.js

---
+++ poolduino.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+
+function on(){
+        var r = confirm("LIGAR BOMBA!\nCONFIRMAR?");
+        if (r == true) {
+        window.location.replace("?ligar")
+        } else {}
+}
+
+function off(){
+        var r = confirm("DESLIGAR BOMBA!\nCONFIRMAR?");
+        if (r == true) {
+        window.location.replace("?desligar")
+        } else {}
+}
+
+function dois(){
+        window.location.replace("?dois")
+}
+
+function quatro(){
+        window.location.replace("?quatro")
+}
+
+function seis(){
+        window.location.replace("?seis")
+}
